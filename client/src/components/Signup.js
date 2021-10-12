7585857f48813650ce118f67a2c0b25804fae3d0
Courtney Wilson <cmwilson132@gmail.com>
2021-10-11 21:47:22 -0500

    added basic signup form - email and password

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -3,7 +3,13 @@ import React from 'react'
 const Signup = () => {
   return (
     <div>
-      <p>add sign up form here</p>
+      <form>
+        <label htmlFor="email">Email:</label>
+        <input type="text" name="email" id="email" placeholder="Enter your email" autoFocus={true}/>
+        <br />
+        <label htmlFor="password">Password:</label>
+        <input type="password" name="password" id="password" placeholder="Enter your password" />
+      </form>
     </div>
   )
 }

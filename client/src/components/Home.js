bb2ee09a670e915fa076dfd4b90362180fc01ddc
Courtney Wilson <cmwilson132@gmail.com>
2021-10-11 22:15:15 -0500

    added spacing on home and in journal entry

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,7 +4,8 @@ const Home = () => {
   return (
     <div>
       <h2>Welcome to Dusty Shoes!</h2>
-      <h3>When you travel, you trek over the dusty grounds of foreign soil. Get out there and get your shoes dusty.</h3>
+      <h3>When you travel, you trek over the dusty grounds of foreign soil.</h3>
+      <h3>Get out there and get your shoes dusty.</h3>
     </div>
   )
 }
